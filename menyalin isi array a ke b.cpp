81fb9de986ba677586cfe4d8cfae3788f52c765e
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-25 22:45:40 +0700

    Add files via upload

---
+++ menyalin isi array a ke b.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+
+int a, S[5]={},C[5];
+printf("0  1  2 3 4\n");
+for(a=0;a<5;a++){
+scanf("%i",&S[a]);
+}
+
+for(a=0;a<5;a++){
+C[a]=S[a];
+}
+
+for(a=0;a<5; a++){
+printf("%2i",C[a]);
+
+}
+
+
+}
+
